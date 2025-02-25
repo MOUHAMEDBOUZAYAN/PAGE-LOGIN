fe3bdd03fb421393b3bdd7557c927a5660ae20bd
mouhamedbouzayan <bouzimohammed177@gmail.com>
2025-02-25 10:57:06 +0000

    Create User model

---
+++ authInMern/server/models/user.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
+const Joi = require('joi');
+const passwordComplexity = require("joi-password-complexity");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        require: true
+    },
+
+    lastName: {
+        type: String,
+        require: true
+    },
+
+    email: {
+        type: String,
+        require: true
+    },
+
+    password: {
+        type: String,
+        require: true
+    },
+
+});
+
+userSchema.methods.generateAuthToken = function () {
+    const token = jwt.sign({ _id: this._id }, process.env.JWTPRIVATEKEY, { expiresIn: "7d" })
+    return token
+};
+
+const User = mongoose.model("user", userSchema);
+
+const validate = (data) => {
+    const shema = Joi.object({
+        firstName:Joi.string().required().label("First Name"),
+        lastName:Joi.string().required().label("First Name"),
+        email:Joi.string().email().required().label("Email"),
+        password: passwordComplexity().requireded.label("Password")
+    });
+    return schema.validate(data)
+};
+
+module.exports = {User,validate};
