6292f1e89d83e6332114f0b378d8f8b8a680add5
mouhamedbouzayan <bouzimohammed177@gmail.com>
2025-02-26 11:45:48 +0000

    Import Routes

--- authInMern/server/index.js
+++ authInMern/server/index.js
@@ -3,13 +3,19 @@ const express = require ('express');
 const app = express();
 const cors = require("cors");
 const connection = require("./db");
-
+const userRoutes = require("./routes/users");
+const authRoutes = require("./routes/auth");
+ 
 // database connection 
 connection()
 
 // middlewares´
 app.use(express.json())
 app.use(cors());
+
+// routes
+app.use("/api/users",userRoutes);
+app.use("/api/auth",authRoutes);
 
 const port = process.env.Port || 8080;
 app.listen(port, ()=> console.log(`Listenning on port ${port}....`));
